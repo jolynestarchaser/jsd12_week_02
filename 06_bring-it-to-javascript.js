0c8c02f44d970381bf9bf94dbbf46f0f3e0cc40b
jolynestarchaser <mrkaokungkub@gmail.com>
2026-03-10 15:56:49 +0700

    Add code

--- 06_bring-it-to-javascript.js
+++ 06_bring-it-to-javascript.js
@@ -1,2 +1,63 @@
 // เอา entity ที่เขียนใน diagram มาเขียนในรูปแบบของ Javascript code
 // เช่น String for product name, number for price
+
+let itemList = [(item_1),(item_2),(item_3)];
+
+let item_1 = {
+    itemname: "Vinyl",
+    item_sku: "sku01",
+    quantity: 5,
+    price : 10000,
+    limited : true,
+    avaliable : true,
+
+};
+
+let item_2 = {
+    itemname: "CD",
+    item_sku: "sku02",
+    quantity: 100,
+    price : 300,
+    limited : true,
+    avaliable : true,
+
+};
+
+let item_3 = {
+    itemname: "Artwork",
+    item_sku: "sku03",
+    quantity: 5,
+    price : 1000,
+    limited : false,
+    avaliable : true,
+
+};
+
+
+let userid = {
+    username: "Preaw",
+    displayname: "Preaw lnwza",
+    email: "test@gmail.com",
+    number: "081234578"
+    password: 1234,
+    address: "Bangkok,Thailand",
+    Artist: false
+
+}
+
+let userid01 = {
+    username: "starchaser",
+    displayname: "sterchaseza",
+    email: "star@gmail.com",
+    number: "555554545"
+    password: 1234
+    ,address: "Trat,thailand",
+    Artist: true
+
+}
+
+const usercart
+user = userid01;
+itemList = [(item_1),(item_2),(item_3)]
+
+console.log(usercart)
